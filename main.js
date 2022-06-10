ed21356509150105d5c9cba7fa2bc2109a283a0f
Vinay2009 <91785104+Vinay2009@users.noreply.github.com>
2022-06-10 17:17:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload() {
+}
+
+function setup() {
+    canvas = createCanvas(350, 350);
+    canvas.center();
+}
+
+function draw(){
+}
+
+function onClick(){
+    save('My Filter Image.png')
+}
